5ccc5be399770e86c135d05fa5a67dc7243da5f9
bnonni <bryanwnonni@gmail.com>
2016-08-18 21:28:54 -0400

    Updates public

---
+++ public/scripts/keys.js
@@ -0,0 +1,9 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBbvDFOaAkC7T3uMTHamWJPJXzI9sItIz0",
+    authDomain: "affinder-e1943.firebaseapp.com",
+    databaseURL: "https://affinder-e1943.firebaseio.com",
+    storageBucket: "",
+  };
+  firebase.initializeApp(config);
+    
