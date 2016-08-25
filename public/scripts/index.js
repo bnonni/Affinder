16dd70d4d1eca283ea14db4e89a7f9efeff2e6ab
bnonni <bryanwnonni@gmail.com>
2016-08-25 17:16:18 -0400

    style.css body

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,12 +1,39 @@
 //Main JavaScript Code
 $(document).ready(function(){
-    $('#submit-btn').parsley().on('click', validateForm)
+    $('.readmore').on('click', showBlog);
+    $('.readless').on('click', hideBlog);
+    $('.readmore-two').on('click', showBlogTwo);
+    $('.less').on('click', showBlogTwo);
+    $('#submit-btn').parsley().on('click', validateForm);
 })
+
+function showBlog(){
+    event.preventDefault();
+    $('#show-this-on-click').slideDown(1); 
+    $('.readmore').hide();
+    $('.readless').show();
+}
+function hideBlog(){
+    event.preventDefault();
+    $('.hide').slideUp(1);
+    $('.readmore').show();
+}
+
+function showBlogTwo(){
+    event.preventDefault();
+    $('#show-this-on-click-two').slideDown(1);
+    $('.readmore-two').hide();
+    $('.less').show();
+}
+
+function hideBlogTwo(){
+    event.preventDefault();
+    $('.hide-two').slideUp(1);
+    $('.readmore-two').show();
+}
 
 function validateForm(){
    event.preventDefault();
-   $('form').parsley().on('field:validated', validateForm){
-       var ok = $('.parsley-error').length === 0;
-       $('.bs-callout-info').toggleClass('hidden', !ok);
-       $('.bs-callout-warning').toggleClass('hidden', ok);
-}}
+   console.log('work');
+   var ok = $('.parsley-error').length === 0;
+}
