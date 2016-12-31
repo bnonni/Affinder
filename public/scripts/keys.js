74c6af4faca73f14118f828f3f6d0e7bc7314363
bnonni <bryanwnonni@gmail.com>
2016-12-31 13:32:20 -0500

    firebase storage

--- public/scripts/keys.js
+++ public/scripts/keys.js
@@ -6,13 +6,17 @@
     storageBucket: "affinder-e1943.appspot.com",
   };
   firebase.initializeApp(config);
+
+var storageRef = storage.ref();
+var storage = firebase.storage();
+
     
 //     $(document).ready(function(){
 //       var recordsAppReference = firebase.database();
 //    recordsAppReference.ref('records').on('value',function(results){
 //       var allRecords = ref.exportVal();
 //       allRecords.forEach(function(rec,index){
-//         $('#myPopup').append("<p>"+rec.contact+"<br>"+rec.email+"<br>"+rec.phone+"<br>"+rec.publisher+"</p>");
+//         $('html').append("<p>"+rec.contact+"<br>"+rec.email+"<br>"+rec.phone+"<br>"+rec.publisher+"</p>");
 // });
 //     })
 // });
